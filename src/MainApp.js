e23d4ae22721392e5ae59317abe72850c919323b
Winfred Selwyn Ooh Azlin <winfredselwyn@gmail.com>
2017-01-14 22:13:10 +0800

    added Redux

---
+++ src/MainApp.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+class MainApp extends Component {
+    render() {
+        return (
+            <View></View>
+        )
+    }
+}
+
+export default MainApp;
