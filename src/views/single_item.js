513daa000497387b62b316c41dd8baec5e5e5169
Winfred Selwyn Ooh Azlin <winfredselwyn@gmail.com>
2017-01-15 22:05:00 +0800

    added Single News Item view

---
+++ src/views/single_item.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SingleItem extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Single news page</h1>
+      </div>
+    );
+  }
+};
+
+export default SingleItem;
